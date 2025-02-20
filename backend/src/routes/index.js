7943f9d274cf2f691c3f6532e36f19a0a14a4688
ParasJain1803 <paras2004jain@gmail.com>
2025-02-20 22:07:18 +0530

    route setup

---
+++ backend/src/routes/index.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import authRoute from "./Auth.route.js";
+
+const routes = [
+    {
+        path: "/auth",
+        route: authRoute
+    }
+]
+const router = Router();
+routes.forEach((curr) => {
+    router.use(curr.path, curr.route)
+})
+
+export default router;
