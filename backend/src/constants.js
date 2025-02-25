bcfb04d4fc5613bbdbf5291d1c27c9c250a5b572
ParasJain1803 <paras2004jain@gmail.com>
2025-02-25 22:30:54 +0530

    jwt token

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,5 +1,5 @@
 export class PublicData {
     static port = process.env.PORT || 3000
     static mongo_uri = process.env.MONGO_URI || 'mongodb://localhost/inventory';
-    
+    static jwt_token = process.env.JWT_TOKEN || "fnsdfhbsvhn@^&#$*&*&!@*!&*($((&%(*))))"
 }
