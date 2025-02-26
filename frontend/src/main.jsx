17eddc311d326f0f4f28394d2f5568fd10193934
ParasJain1803 <paras2004jain@gmail.com>
2025-02-26 21:30:51 +0530

    add header comp from PRIMEREACT

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import { Routes } from "./utils/Route.jsx";
 import { Provider } from "react-redux";
 import { store } from "./utils/Store.jsx";
+import { PrimeReactProvider } from "primereact/api";
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store} />
-    <RouterProvider router={Routes} />
+    <PrimeReactProvider>
+      <Provider store={store}>
+        <RouterProvider router={Routes} />
+      </Provider>
+    </PrimeReactProvider>
   </StrictMode>
 );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-      
-    </div>
+    <>
+    </>
   )
 }
 
