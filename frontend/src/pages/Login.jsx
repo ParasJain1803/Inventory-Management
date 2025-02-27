37b33f9bae85dea31f9f00b7a1d81d95a2b6c55b
ParasJain1803 <paras2004jain@gmail.com>
2025-02-27 23:10:48 +0530

    start login component install formik

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { InputText } from "primereact/inputtext";
+import * as Yup from "yup";
 
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  // name: Yup.string().required("Name is required"),
+  // email: Yup.string().email("Invalid email").required("Email is required"),
+  // password: Yup.string()
+  //   .min(6, "At least 6 characters")
+  //   .required("Password is required"),
+});
+
+const OnSubmitHandler = 
 function Login() {
   return (
-    <>
-    jhi
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center w-full bg-[#eee]">
+      <Formik initialValues={initialValues}>
+        <form className="w-1/3 shadow-md rounded-md p-4 bg-white">
+          <div className="mb-3">
+            <label htmlFor="email">Email</label>
+            <InputText
+              unstyled
+              className="w-full outline-none py-3 px-2 border-[.1px] border-zinc-400 rounded-md"
+              placeholder="Enter email here"
+            />
+          </div>
+        </form>
+      </Formik>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
