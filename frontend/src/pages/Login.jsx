497dcbfe57ce2405bcce84d0e9c40f3265c39e8f
ParasJain1803 <paras2004jain@gmail.com>
2025-02-28 01:11:45 +0530

    login and register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { InputText } from "primereact/inputtext";
+import { Link } from "react-router-dom";
+
+import { Button } from "primereact/button";
+
 import * as Yup from "yup";
 
 const initialValues = {
@@ -10,31 +13,101 @@ const initialValues = {
 };
 
 const validationSchema = Yup.object({
-  // name: Yup.string().required("Name is required"),
-  // email: Yup.string().email("Invalid email").required("Email is required"),
-  // password: Yup.string()
-  //   .min(6, "At least 6 characters")
-  //   .required("Password is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .min(6, "At least 6 characters")
+    .required("Password is required"),
 });
 
-const OnSubmitHandler = 
-function Login() {
+const OnSubmitHandler = async (e, { resetForm }) => {
+  console.log({e});
+  
+};
+const Login = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center w-full bg-[#eee]">
-      <Formik initialValues={initialValues}>
-        <form className="w-1/3 shadow-md rounded-md p-4 bg-white">
-          <div className="mb-3">
-            <label htmlFor="email">Email</label>
-            <InputText
-              unstyled
-              className="w-full outline-none py-3 px-2 border-[.1px] border-zinc-400 rounded-md"
-              placeholder="Enter email here"
-            />
-          </div>
-        </form>
-      </Formik>
-    </div>
+    <>
+      <div className="min-h-screen flex items-center justify-center w-full bg-[#eee]">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={OnSubmitHandler}
+        >
+          {({ values, setFieldValue, handleSubmit }) => (
+            <>
+              <form
+                onSubmit={handleSubmit}
+                className="w-[96%] md:w-[70%] lg:w-1/3 shadow-md rounded-md pt-10 pb-3 px-4 bg-white"
+              >
+                {/* email */}
+                <div className="mb-3 py-1">
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    id="email"
+                    value={values.email}
+                    onChange={(e) => setFieldValue("email", e.target.value)}
+                    className="w-full outline-none py-3 px-2 border-[.1px] border-zinc-400 rounded-lg"
+                    placeholder="Enter email here"
+                  />
+                  <ErrorMessage
+                    component={"p"}
+                    className="'text-red-400 text-sm"
+                    name="email"
+                  />
+                </div>
+                {/* password */}
+                <div className="mb-3 py-1">
+                  <label htmlFor="password">password</label>
+                  <Field
+                    id="password"
+                    value={values.password}
+                    onChange={(e) => setFieldValue("password", e.target.value)}
+                    className="w-full outline-none py-3 px-2 border-[.1px] border-zinc-400 rounded-lg"
+                    placeholder="Enter password here"
+                  />
+                  <ErrorMessage
+                    component={"p"}
+                    className="'text-red-400 text-sm"
+                    name="password"
+                  />
+                </div>
+                {/* submit button */}
+                <div className="mb-3 py-1 flex items-center justify-center">
+                  <Button
+                  // type="submit"
+                  // raised={true}
+                    // disabled={!values.token}
+                    // loading={LoginUserResponse.isLoading}
+                    className="w-full bg-red-500 text-white py-3 px-2 flex items-center justify-center"
+                  >
+                    Submit
+                  </Button>
+                </div>
+                {/* register */}
+                <div className="mb-3 py-1 flex items-center justify-end">
+                  <p className="inline-flex items-center gap-x-1">
+                    {" "}
+                    Don't Have An Account?
+                    <Link className="font-semibold" to={"/register"}>
+                      Register
+                    </Link>
+                  </p>
+                </div>
+                {/* forget password */}
+                <div className="mb-3 py-1 flex items-center justify-end">
+                  <p className="inline-flex items-center gap-x-1">
+                    {" "}
+                    <Link className="font-semibold" to={"#"}>
+                      Forget Password ?
+                    </Link>
+                  </p>
+                </div>
+              </form>
+            </>
+          )}
+        </Formik>
+      </div>
+    </>
   );
-}
+};
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,116 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Link } from "react-router-dom";
 
-function Register() {
+import { Button } from "primereact/button";
+
+import * as Yup from "yup";
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .min(6, "At least 6 characters")
+    .required("Password is required"),
+});
+
+const OnSubmitHandler = async () => {};
+const Register = () => {
   return (
-    <div>
-      register
-    </div>
-  )
-}
+    <>
+      <div className="min-h-screen flex items-center justify-center w-full bg-[#eee]">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={OnSubmitHandler}
+        >
+          {({ values, setFieldValue, handleSubmit }) => (
+            <>
+              <form
+                onSubmit={handleSubmit}
+                className="w-[96%] md:w-[70%] lg:w-1/3 shadow-md rounded-md pt-10 pb-3 px-4 bg-white"
+              >
+                <div className="mb-3 py-1">
+                  <label htmlFor="name">Name</label>
+                  <Field
+                    id="name"
+                    value={values.name}
+                    onChange={(e) => setFieldValue("name", e.target.value)}
+                    className="w-full outline-none py-3 px-2 border-[.1px] border-zinc-400 rounded-lg"
+                    placeholder="Enter name here"
+                  />
+                  <ErrorMessage
+                    component={"p"}
+                    className="'text-red-400 text-sm"
+                    name="email"
+                  />
+                </div>
+                {/* // email */}
+                <div className="mb-3 py-1">
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    id="email"
+                    value={values.email}
+                    onChange={(e) => setFieldValue("email", e.target.value)}
+                    className="w-full outline-none py-3 px-2 border-[.1px] border-zinc-400 rounded-lg"
+                    placeholder="Enter email here"
+                  />
+                  <ErrorMessage
+                    component={"p"}
+                    className="'text-red-400 text-sm"
+                    name="email"
+                  />
+                </div>
+                {/* // password */}
+                <div className="mb-3 py-1">
+                  <label htmlFor="password">password</label>
+                  <Field
+                    id="password"
+                    value={values.password}
+                    onChange={(e) => setFieldValue("password", e.target.value)}
+                    className="w-full outline-none py-3 px-2 border-[.1px] border-zinc-400 rounded-lg"
+                    placeholder="Enter password here"
+                  />
+                  <ErrorMessage
+                    component={"p"}
+                    className="'text-red-400 text-sm"
+                    name="password"
+                  />
+                </div>
+                {/* // submit button */}
+                <div className="mb-3 py-1 flex items-center justify-center">
+                  <Button
+                  type="submit"
+                    // disabled={!values.token}
+                    // loading={LoginUserResponse.isLoading}
+                    className="w-full bg-red-500 text-white py-3 px-2 flex items-center justify-center"
+                  >
+                    Submit
+                  </Button>
+                </div>
+                {/* // register */}
+                <div className="mb-3 py-1 flex items-center justify-end">
+                  <p className="inline-flex items-center gap-x-1">
+                    {" "}
+                    Already Have An Account?
+                    <Link className="font-semibold" to={"/login"}>
+                      Login
+                    </Link>
+                  </p>
+                </div>
+              </form>
+            </>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
